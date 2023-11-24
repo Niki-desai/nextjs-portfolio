bf3f74a0447ee5c73092f8ad53a6bf0cd87c39e3
Niki-desai <nikita.d10.nd@gmail.com>
2023-11-24 12:31:02 +0530

    projects pageone

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -19,77 +19,36 @@ const page = () => {
   //   console.log("ProjectData", JSON.stringify(ProjectData));
   return (
     <>
-      {/* <div className="h-somescreen  items-center justify-center bg-fuchsia-200 mt-12 overflow-hidden">
-        <div class="grid grid-cols-3 gap-7 h-full w-full   rounded-lg shadow-md p-4">
-          <div>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardMedia
-                sx={{ height: 240 }}
-                image="/images/Avatar.png"
-                title="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardMedia
-                sx={{ height: 240 }}
-                image="/images/Avatar.png"
-                title="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
+      <div className="min-h-screen bg-fuchsia-200">
+        <div className="items-center justify-center  mt-9 overflow-hidden">
+          <div class="grid grid-cols-3 gap-7 rounded-lg  p-4">
+            {/* <div className="relative h-somescreen flex items-center justify-center bg-fuchsia-200 mt-12 "> */}
+            {/* <div className="grid grid-cols-3 gap-7 p-4 "> */}
+            {projects &&
+              projects.map((project) => (
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardMedia
+                    sx={{ height: 140 }}
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    title="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {project.projectName}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {project.projectDescription}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button>
+                  </CardActions>
+                </Card>
+              ))}
+            {/* </div> */}
+            {/* </div>  */}
           </div>
-        </div>
-      </div> */}
-
-      <div className="relative h-somescreen flex items-center justify-center bg-fuchsia-200 mt-12 overflow-hidden">
-        <div className="grid grid-cols-3 gap-7 p-4 ">
-          {projects &&
-            projects.map((project) => (
-              <Card sx={{ maxWidth: 345 }}>
-                <CardMedia
-                  sx={{ height: 140 }}
-                  image="/static/images/cards/contemplative-reptile.jpg"
-                  title="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {project.projectName}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {project.projectDescription}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">Share</Button>
-                  {/* <Button size="small">Learn More</Button> */}
-                </CardActions>
-              </Card>
-            ))}
         </div>
       </div>
     </>
@@ -97,3 +56,30 @@ const page = () => {
 };
 
 export default page;
+
+//  {/* <div className="relative h-somescreen flex items-center justify-center bg-fuchsia-200 mt-12 ">
+//         <div className="grid grid-cols-3 gap-7 p-4 ">
+//           {projects &&
+//             projects.map((project) => (
+//               <Card sx={{ maxWidth: 345 }}>
+//                 <CardMedia
+//                   sx={{ height: 140 }}
+//                   image="/static/images/cards/contemplative-reptile.jpg"
+//                   title="green iguana"
+//                 />
+//                 <CardContent>
+//                   <Typography gutterBottom variant="h5" component="div">
+//                     {project.projectName}
+//                   </Typography>
+//                   <Typography variant="body2" color="text.secondary">
+//                     {project.projectDescription}
+//                   </Typography>
+//                 </CardContent>
+//                 <CardActions>
+//                   <Button size="small">Share</Button>
+//                   {/* <Button size="small">Learn More</Button> */}
+//                 </CardActions>
+//               </Card>
+//             ))}
+//         </div>
+//       </div> */}
